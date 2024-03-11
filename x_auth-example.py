c88209d5b9f8d45f0f05ea07afa8e7805acf8e3a
tombrushwood <24531227+tombrushwood@users.noreply.github.com>
2024-03-11 12:03:34 +0000

    Improved x_auth example file

--- x_auth-example.py
+++ x_auth-example.py
@@ -5,10 +5,21 @@
 # - Upgrade your project to basic level access ($100)
 # - copy/paste this file and rename it to x_auth.py
 # - Generate a bearer token for your app, and copy/paste it below
+# - Generate API + access Tokens, and copy/paste them below
+
 
 # TWITTER AUTH V2
 
-# Needed for OAuth 2.0 authentication
+# Needed for basic OAuth 2.0 App-Only authentication (for find_groups.py)
 BEARER_TOKEN = "..."
 
 
+# Needed for OAuth 2.0 with PKCE (authenticating as a user, for get_timeline.py)
+
+# Essentially a user name and password, sometimes called the consumer_key and consumer_secret
+API_KEY = "..." # (API key)
+API_SECRET = "..." # (API secret key)
+
+# Essentially the user you are making the request on behalf of
+ACCESS_TOKEN = "..." # (Access token)
+ACCESS_TOKEN_SECRET = "..." # (Access token secret)
